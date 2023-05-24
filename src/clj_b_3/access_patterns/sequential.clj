57213955a20b6b4f45595890e000082884dbda43
kraulain <kraulain@gmail.com>
2023-05-24 11:14:15 +0100

    Access lists sequentially

--- src/clj_b_3/access_patterns/sequential.clj
+++ src/clj_b_3/access_patterns/sequential.clj
@@ -1,2 +1,13 @@
 (ns clj-b-3.access-patterns.sequential)
 
+;; Arbitrary stable patterns (order is maintained)
+
+;;; List
+
+(def utensils (list :knife :spoon :fork))
+
+(seq utensils) ;; order is maintained
+
+(conj utensils :spatula) ;; new items gets added to front
+
+
