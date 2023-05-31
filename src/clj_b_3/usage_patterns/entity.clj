e8305894d6d084f24b1b664edda34a458888b79f
kraulain <kraulain@gmail.com>
2023-05-31 10:50:41 +0100

    Try out entity usage pattern

--- src/clj_b_3/usage_patterns/entity.clj
+++ src/clj_b_3/usage_patterns/entity.clj
@@ -1,9 +1,10 @@
 (ns clj-b-3.usage-patterns.entity)
 
-;; index usage pattern
+(def user
+  {:name "John Doe"
+   :address "Tamtamm weekend"
+   :height 1.6
+   :date-of-birth #inst "1999-07-18"})
 
-;; entity usage pattern
 
-;; Tuple usage pattern
-
-;; Multi comparison ussage pattern
+(:name user)
